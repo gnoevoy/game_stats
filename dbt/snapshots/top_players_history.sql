1c912cdae50340a2ece92cc6799f480cfa24868a
bars1k <gnoevoyandrey044@gamil.com>
2025-07-01 03:44:21 +0200

    fixing dbt snaphost, changes strategy (checks only player ranks)

--- dbt/snapshots/top_players_history.sql
+++ dbt/snapshots/top_players_history.sql
@@ -7,8 +7,8 @@
    config(
        target_schema='game_stats_analytics',
        unique_key='player_id',
-       strategy='timestamp',
-       updated_at='utc_valid_at',
+       strategy='check',
+       check_cols=["rank"],
    )
 }}
 
